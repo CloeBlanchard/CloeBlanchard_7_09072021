db7be591e71b734277f0b2eb796feb29030405a8
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-11 12:30:50 +0200

    création de la route de la récupération des utilisateurs

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
+// variable
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
+const mysql = require('mysql');
+require('dotenv').config();
 
-// cors
+
+// application
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -11,6 +15,32 @@ app.use((req, res, next) => {
     next();
 });
 
+// body parser
 app.use(bodyParser.json());
 
+// route par défaut
+app.get('/', function(req, res) {
+    return res.send({ message: 'hello world'})
+});
+
+// db connect
+const dbConnection = mysql.createConnection({
+    host: process.env.host,
+    user: process.env.user,
+    database: process.env.db
+});
+dbConnection.connect();
+
+// récupération des utilisateurs
+app.get('/users', function (req, res) {
+    dbConnection.query('SELECT * FROM users', function (error, results, fields) {
+        if (error) throw error;
+        return res.send({ error: false, data: results, message: 'users list.' });
+    });
+});
+
+// set port
+app.listen(3000, function() {
+    console.log('Node app is running on port 3000');
+});
 module.exports = app;
