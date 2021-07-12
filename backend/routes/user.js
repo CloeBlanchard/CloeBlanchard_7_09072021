a640a27e1ce41127a928307e4c6a7f6be2224a54
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-12 09:22:29 +0200

    modification de la route getAllUsers

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,12 @@
 // import d'express
-const expres = require('express');
+const express = require('express');
 // import du router
-const router = expres.Router();
+const router = express.Router();
 // import du controllers des utilisateur
 const userCtrl = require('../controllers/user');
 
 // import du router de récupération de tous les utilisateurs
 router.get('/users', userCtrl.getAllUsers);
+
+// Export du router
+module.exports = router;
