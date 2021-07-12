6c2c9f02610fe6c129cca8ab8beeae27e0f336ff
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-12 10:38:10 +0200

    ajout de la route de création d'un utilisateur

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -16,21 +16,36 @@ var key = cryptoJs.enc.Hex.parse(process.env.key);
 var iv = cryptoJs.enc.Hex.parse(process.env.iv);
 
 // récupération de tous les utilisateurs
-exports.getAllUsers = (req, res) => {
+exports.getAllUsers = (req, res, next) => {
     dbConnection.query('SELECT * FROM users', (err, result) => {
         if (err) throw err;
-        return res.send({ error: false, message: result});
+        return res.send({ error: false, message: result });
     });
 };
 
 // récupération d'un utilisateur avec l'id
-exports.getUser = (req, res) => {
+exports.getUser = (req, res, next) => {
     let user_id = req.params.id;
-    if(!user_id) {
-        return res.status(400).send({ error: true, message: "Veullez fournir l'id de l'utilisateur"});
+    if (!user_id) {
+        return res.status(400).send({ error: true, message: "Veullez fournir l'id de l'utilisateur" });
     }
-    dbConnection.query('SELECT * FROM users WHERE id_user=?', user_id, function(error, results, fields) {
+    dbConnection.query('SELECT * FROM users WHERE id_user=?', user_id, (error, results) => {
         if (error) throw error;
         return res.send({ error: false, message: results[0] });
     });
 }
+
+// création d'un nouvel utilisateur
+exports.signup = (req, res, next) => {
+    const userinfo = req.body.userinfo;
+    // si les infos de l'user ne sont pas correcte
+    if (!userinfo) {
+        return res.status(400).send({ error, message: "Les champs ne sont pas complet" });
+    }
+    // connection à la bdd et insertion des données de l'utilisateur
+    dbConnection.query("INSERT INTO users SET ?", userinfo, (error, results, fields) => {
+        // on renvoie le resultat
+        return res.send({ error: false, data: results, message: "L'utilisateur à bien été crée" });
+    });
+};
+
