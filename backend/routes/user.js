0e9f5d296c75cf951a9be78dbc3f94de14402db2
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-12 09:29:22 +0200

    ajout des routes getUser

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,6 +7,7 @@ const userCtrl = require('../controllers/user');
 
 // import du router de récupération de tous les utilisateurs
 router.get('/users', userCtrl.getAllUsers);
-
+// import du router de récupération d'un utilsateur avec l'id
+router.get('/user/:id', userCtrl.getUser);
 // Export du router
 module.exports = router;
