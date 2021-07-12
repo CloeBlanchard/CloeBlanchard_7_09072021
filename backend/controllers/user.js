343df57358e34c09f3a6af14e8eb650524e83665
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-12 08:38:39 +0200

    modification du ctrls des utilisateurs route getAllUsers

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,16 +1,23 @@
 // import de bcrypt
 const bcrypt = require('bcrypt');
-// import de jsonwebtoken
+// import de jswebtoken
 const jwt = require('jsonwebtoken');
-// import de cryptojs
-const cryptojs = require('crypto-js');
+// import de mysql
+const mysql = require('mysql');
+// import du gestionnaire d'environnement
 require('dotenv').config();
+// import de le connection de mysql
+const db_connection = require('../db_connect');
+// import de cryptojs
+const cryptoJs = require('crypto-js');
 
-var key = cryptojs.enc.Hex.parse(process.env.key);
-var iv = cryptojs.enc.Hex.parse(process.env.iv)
+// variable de cryptojs
+var key = cryptoJs.enc.Hex.parse(process.env.key);
+var iv = cryptoJs.enc.Hex.parse(process.env.iv);
 
-exports.getAllUsers = (req, res, next) => {
-    dbConnection.query('SELECT * FROM users', function (error, results, fields) {
+// récupération d'un utilisateur unique
+exports.getAllUsers = (req, res) => {
+    dbConnection.query('SELECT * FROM users', function (error, results) {
         if (error) throw error;
         return res.send({ error: false, data: results, message: 'users list.' });
     });
