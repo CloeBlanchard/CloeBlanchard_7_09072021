182e6fbd017ac568821b3fb404e40bd9f0fbaf89
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-13 17:30:58 +0200

    ajout de la route de la creation d'une publication

--- backend/routes/publication.js
+++ backend/routes/publication.js
@@ -2,12 +2,14 @@
 const express = require('express');
 // import du router
 const router = express.Router();
+// import de multer
+const multer = require('../middleware/multer-config')
 // import du controllers des publication
 const publicationCtrls = require('../controllers/publication');
 
 
 // récupération d'une publication
 router.get('/publication/:id', publicationCtrls.getOnePublication);
-
+router.post('/publication', multer, publicationCtrls.createPublication);
 // Export du router
 module.exports = router;
