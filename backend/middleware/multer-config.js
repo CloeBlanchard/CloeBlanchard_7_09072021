3be01795b5b10fbd9b297e9a65a71a7a66e0aa6d
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-13 17:32:58 +0200

    ajout de le configuration de multer

---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,25 @@
+// import de multer
+const multer = require('multer');
+
+// dictionnaire du myme type
+const MIME_TYPES = {
+    'image/jpg': 'jpg',
+    'image/jpeg': 'jpg',
+    'image/png': 'png',
+};
+
+// Configuration de multer
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, 'images')
+    },
+    filename: (req, file, callback) => {
+        const name = file.originalname.split(' ').join('_');
+        // géré l'extension du fichier
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
+    },
+});
+
+// Export du multer configurer
+module.exports = multer({ storage }).single('image');
