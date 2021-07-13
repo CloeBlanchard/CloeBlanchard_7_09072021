7ac3a11a702fe609cf863deaca8518f45325519e
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-13 17:30:15 +0200

    ajout de la route de creation d'une publication

--- backend/controllers/publication.js
+++ backend/controllers/publication.js
@@ -10,9 +10,23 @@ exports.getOnePublication = (req, res) => {
         return res.status(400).send({ error: true, message: "veuillez fournir l'id de la publication" });
     };
     // on récupère les infos de la publication et son id
-    dbConnection.query(`SELECT * FROM posts WHERE id=?`, (error, result) => {
+    dbConnection.query(`SELECT * FROM publications WHERE id=?`, (error, result) => {
         if (error) throw error;
         // si pas 
         return res.status(200).json({ error: false, message : result});
     });
+};
+// création d'un publication
+exports.createPublication = (req, res) => {
+       id_user = req.body.id_user;
+       titre = req.body.titre;
+       corps_message = req.body.corps_message;
+       image =  `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
+   dbConnection.query(`INSERT INTO publications SET ?` , {id_user, titre, corps_message, image}, (error, result) => {
+       if (error) {
+           console.log(error)
+           return res.status(400).json ({ error })
+   }
+   return res.status(201).json({ message: ' Votre message a bien été posté !'})
+   })
 };
