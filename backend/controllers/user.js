91aa6094548270523d0adf6b457b7106285afb76
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-13 08:15:55 +0200

    modification de la route login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -60,53 +60,59 @@ exports.login = (req, res) => {
     // envoie de l'email et du mot de passe dans la bdd
     if (req.body.email && req.body.mot_de_passe){
         // connexion à la bdd
-      dbConnection.query('SELECT * FROM users WHERE email=?', req.body.email, (err, result) => {
+      dbConnection.query('SELECT * FROM users WHERE email=?', req.body.email, (error, result) => {
             // Vérifie que le mail correspond à celui envoyer dans la bdd
             if (result.length <= 0){
                 return res.status(500).json({ error: true, message: "L'email est inconnue à la base de donnée"});
             } else {
+                // compare le mdp de la requete et la mdp enregistrer dans la bdd
                 bcrypt.compare(req.body.mot_de_passe, result[0].mot_de_passe)
                 .then(valid => {
+                    // si non valide renvoie une erreur
                     if(!valid){
                         return res.status(500).json({ error: true, message: "Email ou mot de passe incorrect"});
                     } else {
-                        res.status(200).json({
-                            user_id: result[0].id,
-                            nom: result[0].nom,
-                            prenom: result[0].prenom,
-                            token: jwt.sign({
-                                user_id: result[0].id
-                            }, process.env.TOKEN, {
-                                expiresIn: '24h'
-                            })
+                        // si valide retourne l'id de l'utilisateur et le token d'authentification
+                        res.status(200).json({ 
+                            user_id: result[0].id, 
+                            // fonction du jsonwebtoken
+                            token: jwt.sign({ user_id: result[0].id }, process.env.token, 
+                                // expiration du token
+                                { expiresIn: '24h' })
                         });
                     }
                     })
                 .catch(() => {
                     return res.status(500).json({ error })
-                })
-            }
-        })
-    }
-}
+                });
+            };
+        });
+    };
+};
+
 // mise a jour d'un utilisateur
 exports.updateUser = (req, res) => {
     let user_id = req.params.id;
     let userinfo = req.body.userinfo;
+    // si l'id et les infos de l'utilisateur sont incorrecte
     if (!user_id || !userinfo) {
         return res.status(400).send({ error: user, message: "Veullez fournir l'utilisateur and son id" });
     }
+    // connexion à la bdd pour mettre à jour l'utilisateur avec son id en paramaetre
     dbConnection.query("UPDATE users SET ? WHERE id_user=?", [userinfo, user_id], (error, results) => {
         if (error) throw error;
         return res.send({ error: false, data: results, message: "L'utilisateur à bien été mis à jour" });
     });
 };
 
+// suppresion d'un utilisateur
 exports.deleteUser = (req, res) => {
     let user_id = req.params.id;
+    // si l'id de l'utilisateur ne correspond pas
     if (!user_id) {
         return res.status(400).send({ error: false, message: "Veullez fournir l'id de l'utilisateur" });
     }
+    // connexion à la bdd
     dbConnection.query('DELETE FROM users WHERE id_user= ?', [user_id], function (error, results, fields) {
         if (error) throw error;
         return res.send({ erro: false, data: results, message: "L'utilisateur à bien été supprimé" });
