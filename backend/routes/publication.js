088a02c73e4f4b7b78be34e4438d92d3c33364ca
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-14 12:47:05 +0200

    ajout de la route de modification d'une publication

--- backend/routes/publication.js
+++ backend/routes/publication.js
@@ -11,5 +11,6 @@ const publicationCtrls = require('../controllers/publication');
 // récupération d'une publication
 router.get('/publication/:id', publicationCtrls.getOnePublication);
 router.post('/publication', multer, publicationCtrls.createPublication);
+router.put('/publication/:id', multer, publicationCtrls.modifyPublication);
 // Export du router
 module.exports = router;
