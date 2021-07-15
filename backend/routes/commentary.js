5cb943ed6b98af0faa9852c8e2e75ec666ea18ca
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-15 12:49:25 +0200

    ajout de la route de creation d'un commentaire

--- backend/routes/commentary.js
+++ backend/routes/commentary.js
@@ -4,8 +4,15 @@ const express = require('express');
 const router = express.Router();
 // import du controllers des commentaires
 const commentaryCtrls = require('../controllers/commentary');
+// import de multer
+const multer = require('../middleware/multer-config');
 
 // récupération d'un commentaire
 router.get('/commentary/:id', commentaryCtrls.getOneCommentary);
 // récupération de tous les commentaires
 router.get('/commentarys', commentaryCtrls.getAllCommentarys);
+// création d'un commentaire
+router.post('/commentary', multer, commentaryCtrls.createCommentary);
+
+// Export du router
+module.exports = router;
