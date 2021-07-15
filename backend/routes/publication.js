b504aa29f1806be113365da895474fd87b8b3559
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-15 09:53:01 +0200

    ajout de l'authentification sur les routes crud

--- backend/routes/publication.js
+++ backend/routes/publication.js
@@ -6,18 +6,20 @@ const router = express.Router();
 const multer = require('../middleware/multer-config')
 // import du controllers des publication
 const publicationCtrls = require('../controllers/publication');
+// import de l'authentification
+const auth = require('../middleware/auth');
 
 
 // récupération d'une publication
-router.get('/publication/:id', publicationCtrls.getOnePublication);
+router.get('/publication/:id', auth, publicationCtrls.getOnePublication);
 // récupération de toutes les publications
-router.get('/publications', publicationCtrls.getAllPublications);
+router.get('/publications', auth, publicationCtrls.getAllPublications);
 // création d'une publication
-router.post('/publication', multer, publicationCtrls.createPublication);
+router.post('/publication', auth, multer, publicationCtrls.createPublication);
 // modification d'une publication
-router.put('/publication/:id', multer, publicationCtrls.modifyPublication);
+router.put('/publication/:id', auth, multer, publicationCtrls.modifyPublication);
 // suppression d'une publication
-router.delete('/publication/:id', publicationCtrls.deletePublication);
+router.delete('/publication/:id', auth, publicationCtrls.deletePublication);
 
 // Export du router
 module.exports = router;
