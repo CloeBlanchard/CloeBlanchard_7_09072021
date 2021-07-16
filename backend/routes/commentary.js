5e69fa97635a2031f1d5dc8e0428857ec392f48c
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-16 08:52:14 +0200

    ajout de l'authentification des routes

--- backend/routes/commentary.js
+++ backend/routes/commentary.js
@@ -6,17 +6,19 @@ const router = express.Router();
 const commentaryCtrls = require('../controllers/commentary');
 // import de multer
 const multer = require('../middleware/multer-config');
+// import de l'authentification
+const auth = require('../middleware/auth');
 
 // récupération d'un commentaire
-router.get('/commentary/:id', commentaryCtrls.getOneCommentary);
+router.get('/commentary/:id', auth, commentaryCtrls.getOneCommentary);
 // récupération de tous les commentaires
-router.get('/commentarys', commentaryCtrls.getAllCommentarys);
+router.get('/commentarys', auth, commentaryCtrls.getAllCommentarys);
 // création d'un commentaire
-router.post('/commentary', multer, commentaryCtrls.createCommentary);
+router.post('/commentary', auth, multer, commentaryCtrls.createCommentary);
 // modification d'un commentaire
-router.put('/commentary/:id', multer, commentaryCtrls.modifyCommentary);
+router.put('/commentary/:id', auth, multer, commentaryCtrls.modifyCommentary);
 // suppression d'un commentaire
-router.delete('/commentary/:id', commentaryCtrls.deleteCommentary);
+router.delete('/commentary/:id', auth, commentaryCtrls.deleteCommentary);
 
 // Export du router
 module.exports = router;
