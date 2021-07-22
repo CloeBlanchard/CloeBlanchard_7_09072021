87378f34ca264e600bae842a536b3cc93637b5a9
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-22 17:23:02 +0200

    modification du bton

--- frontend/groupomania/src/components/Signup.vue
+++ frontend/groupomania/src/components/Signup.vue
@@ -23,8 +23,7 @@
             <input type="text" name="mdp" id="mdp_inscription" class="formulaire_input" pattern="[A-Z || a-z0-9]{2,100}" required>
         </div>
 
-        <input id="testBtn" type="button" @click="displayInConsole" value="Cliquer pour afficher dans la console"/>
-        <button id="envoyer_formulaire" @click="inscription" type="submit" name="envoyer_formulaire">Valider l'inscription</button>
+         <button id="envoyer_formulaire" type="submit" name="envoyer_formulaire">Valider l'inscription</button> 
     </form>
   </div>
 </template>
@@ -40,20 +39,8 @@ export default {
       errors: []
     };
   },
-  computed: {
-    joinTheNames() {
-            const nom = document.getElementById("nom_inscription").value;
-            const prenom = document.getElementById("prenom_inscription").value;
-            const email = document.getElementById("mail_inscription").value;
-            const password = document.getElementById("mdp_inscription").value;
-      return `${nom} | ${prenom} | ${email} | ${password}`;
-    },
-  },
   // fonction de creation de l'utilisateur
   methods: {
-    displayInConsole() {
-      console.log(this.joinTheNames);
-    },
     inscription() {
       const nom = document.getElementById("nom_inscription").value;
       const prenom = document.getElementById("prenom_inscription").value;
@@ -65,7 +52,12 @@ export default {
         "email": email,
         "mot_de_passe": password
       })
-      .then(response => {console.log(response);})
+      .then(response => {
+        if (response.status === 201) {
+          location.href = '/login'
+        }
+        console.log(response);
+      })
       .catch(e => {
         this.errors.push(e)
       })
@@ -74,15 +66,4 @@ export default {
     }
   }
 }
-      // axios({
-      //       methods: 'POST',
-      //       url: 'http://localhost:3000/api/auth/signup',
-      //       data: {nom, prenom, mail, password},
-      //   })
-      //   .then((response) => {
-      //       console.log(response);
-      //   })
-      //   .catch( error => {
-      //       console.log(error);
-      //   })
 </script>
