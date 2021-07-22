f2a7c4185a59d280258fff7cd99d10045d7ac152
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-22 17:23:57 +0200

    ajout de la method de connexion d'un utilisateur

--- frontend/groupomania/src/components/Login.vue
+++ frontend/groupomania/src/components/Login.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="Login">
-    <form>
+    <img id="logo" alt="Vue logo" src="../assets/logo.svg">
+    <h1>Connectez-vous</h1>
+    <form @submit.prevent="connection()">
         <div id="formulaire_mail">
             <label>Adresse mail : </label>
             <input type="text" name="mail" id="mail_connexion" class="formulaire_input" required>
@@ -16,10 +18,37 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Login',
-  props: {
-    msg: String
+  // Instanciation des variables
+  data() {
+    return {
+      errors: []
+    };
+  },
+  // fonction de creation de l'utilisateur
+  methods: {
+    connection() {
+      const email = document.getElementById("mail_connexion").value;
+      const password = document.getElementById("mdp_connexion").value;
+      axios.post(`http://localhost:3000/api/auth/login`, {
+        "email": email,
+        "mot_de_passe": password
+      })
+      .then(response => {
+        // if (response.status === 201) {
+        //   location.href = '/publication'
+        // }
+        console.log(response);
+      })
+      .catch(e => {
+        this.errors.push(e)
+      })
+
+  
+    }
   }
 }
 </script>
