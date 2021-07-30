b2b63a101fa5579170c6508fa2843ee61321028c
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-31 00:35:52 +0200

    mise en place du css et a finir

--- frontend/groupomania/src/components/Profil.vue
+++ frontend/groupomania/src/components/Profil.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="Profile">
-    <p>{{this.$user.nom}}</p>
-    <p>{{this.$user.prenom}}</p>
-
-    <button @click="logOut()">Déconnexion</button>
-    <button @click="deleteUser()">Supprimer le compte</button>
+  <div>
+    <img id="logo" alt="Vue logo" src="../assets/logo.svg">
+    <h1 class="profil">Votre profile</h1>
+    <div class="profil_container">
+      <p class="info_profil">Votre nom :</p>
+      <p class="info_profil2">{{this.$user.nom}}</p>
+      <p class="info_profil">Votre prénom :</p>
+      <p class="info_profil2">{{this.$user.prenom}}</p>
     </div>
+      <p class="info_profil3">Pour vous déconnecter :</p>
+      <button class="btn_deconnexion" @click="logOut()">Déconnexion</button>
+      <p class="info_profil3">Pous supprimer votre compte :</p>
+      <button class="btn_suppresion" @click="deleteUser()">Suppression</button>
     
+    </div>
 </template>
 
 <script>
@@ -54,5 +61,62 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
+.profil_container {
+  background-color: #d1515a;
+  border-radius: 10px;
+  padding: 1px;
+  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.39), -0.4em 0 0.4em rgba(0, 0, 0, 0.39);
+}
+.info_profil {
+  font-size: 25px;
+  color: lightgrey;
+
+}
+.info_profil2, .info_profil3 {
+  font-size: 23px;
+  color: #091f43;
+}
+.info_profil2:hover {
+  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.39), -0.4em 0 0.4em rgba(0, 0, 0, 0.39);
+  border-radius: 10px;
+  font-size: 25px;
+  padding: 8px;
+}
+.btn_deconnexion, .btn_suppresion {
+  padding: 10px;
+  font-size: 20px;
+  border-radius: 10px;
+  color: lightgrey;
+  background-color: #091f43;
+  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.39), -0.4em 0 0.4em rgba(0, 0, 0, 0.39);
+}
+.btn_deconnexion:hover, .btn_suppresion:hover {
+  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.534), -0.4em 0 0.4em rgba(0, 0, 0, 0.534);
+  font-size: 23px;
+}
+@media screen and (min-width: 500px) {
+  .profil_container {
+    margin-left: 40px;
+    margin-right: 40px;
+  }
+}
+@media screen and (min-width: 620px) {
+  .profil_container {
+    margin-left: 80px;
+    margin-right: 80px;
+  }
+}
+@media screen and (min-width: 768px) {
+  .profil_container {
+    margin-left: 150px;
+    margin-right: 150px;
+  }
+}
+@media screen and (min-width: 868px) {
+  .profil_container {
+    margin-left: 200px;
+    margin-right: 200px;
+  }
+}
 </style>
