a575da8f7ab45f60130cd569fd74547154e868a3
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-31 12:22:03 +0200

    mise en forme du document

--- frontend/groupomania/src/components/Groupomania.vue
+++ frontend/groupomania/src/components/Groupomania.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="Groupomania">
-    <img id="logo" alt="Vue logo" src="../assets/logo.svg">
+    <img id="logo" alt="Vue logo" src="../assets/logo.svg" />
     <h1 class="accueil">Bienvenue sur le réseau social de votre entreprise</h1>
     <div class="redirection">
-      <p class="inscription">Vous pouvez vous inscrire ici :</p><button class="btn_inscrit"><a href="/Signup">Inscription</a></button>
-      <p class="connexion">Si vous êtes déjà inscrit, vous pouvez vous connecter ici :</p><button class="btn-connecte"><a href="/Login">Connexion</a></button>
+      <p class="inscription">Vous pouvez vous inscrire ici :</p>
+      <button class="btn_inscrit"><a href="/Signup">Inscription</a></button>
+      <p class="connexion">
+        Si vous êtes déjà inscrit, vous pouvez vous connecter ici :
+      </p>
+      <button class="btn-connecte"><a href="/Login">Connexion</a></button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Groupomania',
-}
+  name: "Groupomania",
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -25,14 +29,16 @@ a {
   text-decoration: none;
   font-size: 20px;
 }
-.btn_inscrit a, .btn-connecte a {
+.btn_inscrit a,
+.btn-connecte a {
   color: white;
 }
 .accueil {
   font-size: 25px;
   color: #091f43;
 }
-.inscription, .connexion {
+.inscription,
+.connexion {
   font-size: 20px;
   color: white;
 }
@@ -41,16 +47,21 @@ a {
   padding: 15px;
   border-radius: 20px;
   padding-bottom: 30px;
-  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.356), -0.4em 0 0.4em rgba(0, 0, 0, 0.356);
+  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.356),
+    -0.4em 0 0.4em rgba(0, 0, 0, 0.356);
 }
-.btn_inscrit, .btn-connecte {
+.btn_inscrit,
+.btn-connecte {
   padding: 10px;
   background-color: #091f43;
   border-radius: 10px;
-  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.527), -0.4em 0 0.4em rgba(0, 0, 0, 0.431);
+  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.527),
+    -0.4em 0 0.4em rgba(0, 0, 0, 0.431);
 }
-.btn_inscrit:hover, .btn-connecte:hover {
-  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.65), -0.4em 0 0.4em rgba(0, 0, 0, 0.65);
+.btn_inscrit:hover,
+.btn-connecte:hover {
+  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.65),
+    -0.4em 0 0.4em rgba(0, 0, 0, 0.65);
   font-size: 23px;
 }
 @media screen and (min-width: 614px) {
@@ -63,7 +74,8 @@ a {
     margin-left: 20px;
     margin-right: 20px;
   }
-  .inscription, .connexion {
+  .inscription,
+  .connexion {
     font-size: 25px;
   }
 }
