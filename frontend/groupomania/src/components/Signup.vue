2879eb571aabef96d8d352dde1bbf68615772401
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-31 12:12:43 +0200

    mise en place du css

--- frontend/groupomania/src/components/Signup.vue
+++ frontend/groupomania/src/components/Signup.vue
@@ -1,29 +1,29 @@
 <template>
   <div class="hello">
     <img id="logo" alt="Vue logo" src="../assets/logo.svg">
-    <h1>Inscrivez-vous</h1>
-    <form @submit.prevent="inscription()">
+    <form class="formulaire" @submit.prevent="inscription()">
+      <h1 class="accueil">Inscrivez-vous</h1>
         <div id="formulaire_nom">
-            <label>Nom : </label>
-            <input type="text" name="nom" id="nom_inscription" class="formulaire_input" pattern="[A-Z || a-z0-9]{2,100}" required>
+            <p><label  class="nom">Nom : </label></p>
+            <p><input type="text" name="nom" id="nom_inscription" class="formulaire_input" pattern="[A-Z || a-z0-9]{2,100}" required></p>
         </div>
 
         <div id="formulaire_prenom">
-            <label>Prénom : </label>
-            <input type="text" name="prenom" id="prenom_inscription" class="formulaire_input" pattern="[A-Z || a-z0-9]{2,100}" required>
+            <p><label  class="prenom">Prénom : </label></p>
+            <p><input type="text" name="prenom" id="prenom_inscription" class="formulaire_input" pattern="[A-Z || a-z0-9]{2,100}" required></p>
         </div>
 
         <div id="formulaire_mail">
-            <label>Adresse mail : </label>
-            <input type="text" name="mail" id="mail_inscription" class="formulaire_input" pattern="[a-z0-9._%+-]+@[a-z0-9._%+-]+\.[a-z]{2,50}" required>
+            <p><label  class="mail">Adresse mail : </label></p>
+            <p><input type="text" name="mail" id="mail_inscription" class="formulaire_input" pattern="[a-z0-9._%+-]+@[a-z0-9._%+-]+\.[a-z]{2,50}" required></p>
         </div>
 
         <div id="formulaire_mdp">
-            <label>Mot de passe : </label>
-            <input type="text" name="mdp" id="mdp_inscription" class="formulaire_input" pattern="[A-Z || a-z0-9]{2,100}" required>
+            <p><label  class="password">Mot de passe : </label></p>
+            <p><input type="text" name="mdp" id="mdp_inscription" class="formulaire_input" pattern="[A-Z || a-z0-9]{2,100}" required></p>
         </div>
 
-         <button id="envoyer_formulaire" type="submit" name="envoyer_formulaire">Valider l'inscription</button> 
+         <button class="btn-connecte" id="envoyer_formulaire" type="submit" name="envoyer_formulaire">Valider l'inscription</button> 
     </form>
   </div>
 </template>
@@ -69,3 +69,76 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.formulaire {
+  margin-top: 50px;
+  background-color: #d1515a;
+  padding: 15px;
+  border-radius: 20px;
+  padding-bottom: 30px;
+  box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.356), -0.4em 0 0.4em rgba(0, 0, 0, 0.356);
+}
+.accueil {
+  font-size: 30px;
+}
+.formulaire_input {
+  font-size: 20px;
+  border-radius: 30px;
+}
+.nom, .prenom, .mail, .password {
+  font-size: 25px;
+  color: #091f43;
+}
+.btn-connecte {
+  margin-top: 15px;
+  color: lightgrey;
+  padding: 10px;
+  font-size: 20px;
+}
+.btn-connecte:hover {
+  font-size: 22px;
+}
+@media screen and (min-width: 500px) {
+  .formulaire {
+    margin-left: 40px;
+    margin-right: 40px;
+  }
+}
+@media screen and (min-width: 600px) {
+  .formulaire {
+    margin-left: 80px;
+    margin-right: 80px;
+  }
+}
+@media screen and (min-width: 768px) {
+  .formulaire {
+    margin-left: 140px;
+    margin-right: 140px;
+  }
+}
+@media screen and (min-width: 900px) {
+  .formulaire {
+    margin-left: 180px;
+    margin-right: 180px;
+  }
+}
+@media screen and (min-width: 1000px) {
+  .formulaire {
+    margin-left: 240px;
+    margin-right: 240px;
+  }
+}
+@media screen and (min-width: 1180px) {
+  .formulaire {
+    margin-left: 340px;
+    margin-right: 340px;
+  }
+}
+@media screen and (min-width: 1200px) {
+  .formulaire {
+    margin-left: 380px;
+    margin-right: 380px;
+  }
+}
+</style>
