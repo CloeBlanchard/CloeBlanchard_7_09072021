43d432e8732033a9947ed57b7babfe7d563d4259
CloeBlanchard <cloe.blanchard83@gmail.com>
2021-07-31 11:27:22 +0200

    mise en place du css sur une publication et les commentaires reste a faire l'affichage du formulaire de modification

--- frontend/groupomania/src/components/OnePublication.vue
+++ frontend/groupomania/src/components/OnePublication.vue
@@ -1,19 +1,22 @@
 <template>
     <div class="one_publication">
-        <button><a href="/affichagePublication">Aller aux publications</a></button>
+        <img id="logo" alt="Vue logo" src="../assets/logo.svg">
+        <p><button class="redirection_create"><a href="/affichagePublication">Retour aux publications</a></button></p>
         <!-- si la publication n'est pas modififié -->
-        <div class="publication" v-if="!update">
+        <div class="Publication" v-if="!update">
                 <!-- affichage du titre -->
-                <h2 class="titre">titre: {{this.publications.titre}}</h2>
+                <h2 class="parametre">Titre: </h2>
+                <p class="parametre2">{{this.publications.titre}}</p>
                 <!-- affichage du corps de message -->
-                <p class="corps_message">corps de la publication :{{this.publications.corps_message}}</p>
+                <p class="parametre">Corps de la publication :</p>
+                <p class="parametre2">{{this.publications.corps_message}}</p>
                 <!-- affichage de l'image -->
-                <p>image</p>
-                <img :src="this.publications.image">
+                <p class="parametre">Image</p>
+                <img :src="this.publications.image" alt="image choisie" class="taille">
         </div>
         
         <!-- si la publication est modifié -->
-        <form class="publication" v-if="update">
+        <form v-if="update">
             <!-- update du titre -->
             <label for="update_titre">Améliorer le titre :</label>
             <input type="text" id="update_titre" v-model="publications.titre">
@@ -27,23 +30,23 @@
             <input type="file" id="update_image" @change="configImage"/>
         </form>
 
-        <button v-if="authorized && !update" @click="update = true">Modifier</button>
-        <button v-if="update" @click="update = false">Annuler les changements</button>
-        <button v-if="update" @click.prevent="updatePublication">Publier les changements</button>   
-        <button v-if="update" @click="deletePublication()">Suppression de la publication</button>
-
-        <form @submit.prevent="addCommentary()">
-            <label for="commentaire">Laisser un commmentaire :</label>
-            <input name="nouveau_commentaire" id="commentaire" required/>
-            <button type="submit" id="envoie_commentaire">Envoyer</button>
-        </form>
-
-        <div class="commentaire" v-for="commentary in commentarys" :key="commentary.id">
-            <span class="info_commentaire">Crée le {{dateFormat(commentary.date)}} par {{commentary.prenom}} {{commentary.nom}} : </span>
-            {{commentary.corps_commentaire}}
-            <button @click="deleteCommentary(commentary.id)" v-if="commentary.id_user == $user.id || $user.roleAdmin == 1" :key="commentary.id">Supprimer le commentaire</button>
+        <div class="Commentaire" v-for="commentary in commentarys" :key="commentary.id">
+            <p class="parametre">Crée le {{dateFormat(commentary.date)}}</p>
+            <p class="parametre2">Par {{commentary.prenom}} {{commentary.nom}} : </p>
+            <p  class="parametre3">"{{commentary.corps_commentaire}}"</p>
+            <button class="btn_envoie" @click="deleteCommentary(commentary.id)" v-if="commentary.id_user == $user.id || $user.roleAdmin == 1" :key="commentary.id">Supprimer le commentaire</button>
         </div>
 
+        <form class="Commentaire" @submit.prevent="addCommentary()">
+            <p class="parametre"><label for="commentaire">Laisser un commmentaire :</label></p>
+            <p><input name="nouveau_commentaire" id="commentaire" required/></p>
+            <button class="btn_envoie_commentaire" type="submit" id="envoie_commentaire">Envoyer</button>
+        </form>
+
+        <p><button class="btn_envoie" v-if="authorized && !update" @click="update = true">Modifier la publication</button></p>
+        <p><button class="btn_envoie" v-if="update" @click="update = false">Annuler les changements</button></p>
+        <p><button class="btn_envoie" v-if="update" @click.prevent="updatePublication">Publier les changements</button></p>
+        <p><button class="btn_envoie" v-if="!update" @click="deletePublication()">Suppression de la publication</button></p>
     </div>
 </template>
 
@@ -188,3 +191,86 @@ export default {
     }
 }
 </script>
+
+<style>
+.Publication, .Commentaire {
+    background-color: #d1515a;
+    padding: 20px;
+    margin-bottom: 20px;
+    border-radius: 20px;
+    box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.356), -0.4em 0 0.4em rgba(0, 0, 0, 0.356);
+}
+.parametre {
+    font-size: 25px;
+    color: #091f43;
+    font-weight: bold;
+}
+.parametre2 {
+    font-size: 23px;
+    color: lightgrey;
+    font-weight: bold;
+}
+.parametre3 {
+    font-size: 20px;
+    font-weight: bold;
+}
+.btn_envoie, .btn_envoie_commentaire {
+    font-size: 20px;
+    margin-bottom: 30px;
+    background-color: #091f43;
+    color: lightgrey;
+    border-radius: 10px;
+    padding: 10px;
+    box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.527), -0.4em 0 0.4em rgba(0, 0, 0, 0.431);
+}
+.btn_envoie_commentaire {
+    margin-top: 10px;
+    margin-bottom: 0px;
+}
+#commentaire {
+    font-size: 20px;
+    border-radius: 30px;
+    color: black;
+    box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.527), -0.4em 0 0.4em rgba(0, 0, 0, 0.431);
+}
+.btn_envoie:hover, .btn_envoie_commentaire:hover {
+    box-shadow: 6px 6px 6px rgba(0, 0, 0, 0.65), -0.4em 0 0.4em rgba(0, 0, 0, 0.65);
+    font-size: 23px;
+}
+@media screen and (min-width: 568px) {
+    .Publication, .Commentaire {
+        margin-left: 30px;
+        margin-right: 30px;
+    }
+}
+@media screen and (min-width: 650px) {
+    .Publication, .Commentaire {
+        margin-left: 60px;
+        margin-right: 60px;
+    }
+}
+@media screen and (min-width: 790px) {
+    .Publication, .Commentaire {
+        margin-left: 130px;
+        margin-right: 130px;
+    }
+}
+@media screen and (min-width: 930px) {
+    .Publication, .Commentaire {
+        margin-left: 190px;
+        margin-right: 190px;
+    }
+}
+@media screen and (min-width: 1060px) {
+    .Publication, .Commentaire {
+        margin-left: 230px;
+        margin-right: 230px;
+    }
+}
+@media screen and (min-width: 1200px) {
+    .Publication, .Commentaire {
+        margin-left: 290px;
+        margin-right: 290px;
+    }
+}
+</style>
