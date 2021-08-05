7c6d6266c455c16f317803830c79498cd4fd4472
CloeBlanchard <76440536+CloeBlanchard@users.noreply.github.com>
2021-08-05 22:26:11 +0200

    ajout auth routes user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,19 +4,21 @@ const express = require('express');
 const router = express.Router();
 // import du controllers des utilisateur
 const userCtrl = require('../controllers/user');
+// import de l'authentification
+const auth = require('../middleware/auth')
 
 // import du router de récupération de tous les utilisateurs
-router.get('/users', userCtrl.getAllUsers);
+router.get('/users', auth, userCtrl.getAllUsers);
 // import du router de récupération d'un utilsateur avec l'id
-router.get('/user/:id', userCtrl.getUser);
+router.get('/user/:id', auth, userCtrl.getUser);
 // import de la creation d'un nouvel utilisateur
 router.post('/signup', userCtrl.signup);
 // import de la connexion d'un utilisateur
 router.post('/login', userCtrl.login);
 // import de la modification d'un utilisateur
-router.put('/user/:id', userCtrl.updateUser);
+router.put('/user/:id', auth, userCtrl.updateUser);
 // import de la suppression d'un utilisateur
-router.delete('/user/:id', userCtrl.deleteUser)
+router.delete('/user/:id', auth, userCtrl.deleteUser)
 
 // Export du router
 module.exports = router;
